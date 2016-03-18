e88ae84dca5a7dfdcbad74b4e4f0bf705b436605
Raoul Scarazzini <rasca@redhat.com>
2016-03-18 09:01:23 +0100

    Fix ENVIRONMENTDIR for sub-scripts launching

--- tripleo-director-installer/tripleo-director-installer.sh
+++ tripleo-director-installer/tripleo-director-installer.sh
@@ -4,7 +4,7 @@ source $1/environment &> /dev/null
 
 if [ $? -eq 0 ]
  then
-  export ENVIRONMENT=$1
+  export ENVIRONMENTDIR=$1
   if [ "$OPENSTACK_VERSION" != "osp8" -a "$OPENSTACK_VERSION" != "osp7" -a "$OPENSTACK_VERSION" != "mitaka" ]
    then
     echo "OPENSTACK_VERSION must be 'osp7', 'osp8' or 'mitaka'."
@@ -17,16 +17,16 @@ fi
 
 set -e
 
+WORKINGDIR=$(dirname $0)
+cd $WORKINGDIR
+
 # If provisioning is declared, then we provide the undercloud
 if [ "x$PROVISION_SCRIPT" != "x" ]
  then
   echo "###############################################"
   echo "$(date) Provisioning $UNDERCLOUD (root)"
-  ./$ENVIRONMENT/$PROVISION_SCRIPT
+  $ENVIRONMENTDIR/$PROVISION_SCRIPT
 fi
-
-WORKINGDIR=$(dirname $0)
-cd $WORKINGDIR
 
 echo "###############################################"
 echo "$(date) Uploading undercloud preparation scripts $UNDERCLOUD (root)"
@@ -42,14 +42,14 @@ $SSH root@$UNDERCLOUDIP ./undercloud-repos-$OPENSTACK_VERSION\.sh
 
 echo "###############################################"
 echo "$(date) Uploading undercloud scripts $UNDERCLOUD (root)"
-$SCP -r scripts/undercloud-install.sh scripts/overcloud-{images-$OPENSTACK_VERSION,introspection,deploy,post}.sh scripts/{opensink,follow-events.py} $ENVIRONMENT/{environment,undercloud.conf,instackenv.json,network-environment.yaml,nic-configs} stack@$UNDERCLOUDIP:
+$SCP -r scripts/undercloud-install.sh scripts/overcloud-{images-$OPENSTACK_VERSION,introspection,deploy,post}.sh scripts/{opensink,follow-events.py} $ENVIRONMENTDIR/{environment,undercloud.conf,instackenv.json,network-environment.yaml,nic-configs} stack@$UNDERCLOUDIP:
 
 # If SSL is enabled copy files
 if [ "x$SSL_ENABLE" != "x" ]
  then
   echo "###############################################"
   echo "$(date) Uploading SSL configuration $UNDERCLOUD (stack)"
-  $SCP scripts/undercloud-ssl.sh $ENVIRONMENT/undercloud.pem stack@$UNDERCLOUDIP:
+  $SCP scripts/undercloud-ssl.sh $ENVIRONMENTDIR/undercloud.pem stack@$UNDERCLOUDIP:
   $SSH stack@$UNDERCLOUDIP ./undercloud-ssl.sh
 fi
 
@@ -72,7 +72,7 @@ if [ "x$INTROSPECTION_POST_SCRIPT" != "x" ]
  then
   echo "###############################################"
   echo "$(date) Executing $INTROSPECTION_POST_SCRIPT (stack)"
-  $SCP $ENVIRONMENT/$INTROSPECTION_POST_SCRIPT stack@$UNDERCLOUDIP:
+  $SCP $ENVIRONMENTDIR/$INTROSPECTION_POST_SCRIPT stack@$UNDERCLOUDIP:
   $SSH stack@$UNDERCLOUDIP ./$INTROSPECTION_POST_SCRIPT
 fi
 
@@ -81,7 +81,7 @@ if [ "x$SSL_ENABLE" != "x" ]
  then
   echo "###############################################"
   echo "$(date) Uploading SSL configuration $UNDERCLOUD (stack)"
-  $SCP scripts/overcloud-ssl.sh $ENVIRONMENT/{cloudname,enable-tls,inject-trust-anchor}.yaml $ENVIRONMENT/overcloud-cacert.pem stack@$UNDERCLOUDIP:
+  $SCP scripts/overcloud-ssl.sh $ENVIRONMENTDIR/{cloudname,enable-tls,inject-trust-anchor}.yaml $ENVIRONMENTDIR/overcloud-cacert.pem stack@$UNDERCLOUDIP:
   $SSH stack@$UNDERCLOUDIP ./overcloud-ssl.sh
 fi
 
@@ -91,4 +91,4 @@ $SSH stack@$UNDERCLOUDIP ./overcloud-deploy.sh
 
 echo "###############################################"
 echo "$(date) Starting overcloud post operations"
-$SSH root@$UNDERCLOUDIP ./overcloud-post.sh
+$SSH stack@$UNDERCLOUDIP ./overcloud-post.sh
